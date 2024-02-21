c2e5e369efa6025247abc81aebdc223d817d6c4f
jjoung1128 <jinwoong.joung@gmail.com>
2024-02-20 22:05:16 -0600

    Add navigation bar and footer components***
    ***Add featured story component***
    
    ***Delete unnecessary index.ts file***
    
    ***Add blog page and page load function***
    
    ***Update layout.svelte to include navigation bar and footer

---
+++ src/lib/components/navigation-bar/navigation-bar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import StarGraph from '$lib/assets/star-graph.svg';
+</script>
+
+<div class="flex justify-center">
+	<Navbar class="max-w-6xl">
+		<NavBrand href="/">
+			<img src={StarGraph} class="me-3 h-6 sm:h-9" alt="Star Nest Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>Star Nest</span
+			>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl>
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import FeaturedStory from '$lib/components/featured-story/featured-story.svelte';
+	import EagleNebulaExploration from '$lib/assets/eagle-nebula-exploration.webp';
+	import Astronaut from '$lib/assets/astronaut.svg';
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span>
-		Change a few things up and try submitting again.
-	</Alert>
-</div>
+<section class="flex justify-center p-4">
+	<FeaturedStory
+		image={EagleNebulaExploration}
+		title="Illuminating the Cosmos: The Tech Behind 'Star Nest'"
+		subtitle="Exploring the Svelte Framework's Role in Crafting an Interactive Journey Through the Eagle Nebula"
+		author="Jinwoong Joung & Juseung Park"
+		avatar={Astronaut}
+		date="Feb 20, 2024"
+		readingTime="5 min"
+		href="/blog/illuminating-the-cosmos"
+	/>
+</section>
